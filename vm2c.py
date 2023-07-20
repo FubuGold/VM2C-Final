e0a9faa6d10806c49965a864fbfd407417002a9b
Vòng Chí Văn <48863061+ChiVan222@users.noreply.github.com>
2023-07-20 10:26:01 +0700

    Create vm2c.py

---
+++ vm2c.py
@@ -0,0 +1,13 @@
+import gurobipy
+import gurobipy as gp  # import the installed package
+from gurobipy import GRB
+params = {
+"WLSACCESSID": 'f164f691-1d84-4092-a810-e324efa273f0',
+"WLSSECRET": '52be21e1-ad4a-439a-9963-1d8af268ab0e',
+"LICENSEID": 2395694,
+}
+env = gp.Env(params=params)
+
+# Create the model within the Gurobi environment
+model = gp.Model(env=env)
+model.addVar()
