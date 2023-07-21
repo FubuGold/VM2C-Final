450d65ff43f372d68d9124db101b4fbd41d4a8d0
Vòng Chí Văn <48863061+ChiVan222@users.noreply.github.com>
2023-07-21 22:57:01 +0700

    balance

--- Balance-Rating.py
+++ Balance-Rating.py
@@ -1,6 +1,11 @@
 import numpy as np 
+import matplotlib.pyplot as plt 
 def Load_Result(data_path): 
-    n = 55
+    n = 0 
+    if(data_path == 2) : 
+       n = 55
+    if(data_path == 1): 
+       n =  17 
     path = f'result_data_{data_path}_part_a.txt'  
     check_list = np.zeros((n,2),dtype=int)
     with open(path,"r") as f : 
@@ -14,5 +19,14 @@ def Load_Result(data_path):
             else : 
                 check_list[id-1][1] += 1 
     np.savetxt(f"Balance_Result_{data_path}_part_a",check_list.astype(int),fmt='%d')
+def Balance(balance) : 
+      value = []
+      for i in range(len(balance)) : 
+        value.append(balance[i][0] + balance[i][1])
+      return value
 if __name__ == "__main__": 
-    Load_Result(data_path=2)
+    Load_Result(2)
+    balance = np.loadtxt("Balance_Result_2_part_a")
+    value = Balance(balance)
+    plt.hist(value)
+    plt.show()
