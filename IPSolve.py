5f51a9ef2c81f05964d9fe25ebe767d8460c8b90
FubuGold <trangiahuy211207@gmail.com>
2023-07-21 13:55:00 +0700

    obj

--- IPSolve.py
+++ IPSolve.py
@@ -3,8 +3,16 @@ import gurobipy as gp  # import the installed package
 from gurobipy import GRB
 
 # --------------------------------
-# Create constant
+# Create constant, variable
 N_CHAIN, N_SHIFT, N_SKILL = 3,3,3
+skill = []
+timetable = []
+chain_need = []
+
+id_to_code = {}
+id_to_skill = {0 : "Rot", 1 : "May_dong_hop", 2 : "Pallet"}
+
+shift_count = None
 
 # --------------------------------
 # Create model
@@ -17,12 +25,7 @@ env = gp.Env(params=params)
 
 # --------------------------------
 # Read input
-skill = []
-timetable = []
-chain_need = []
 
-id_to_code = {}
-id_to_skill = {0 : "Rot", 1 : "May_dong_hop", 2 : "Pallet"}
 # Rot - May_dong_hop - Pallet
 with open("FormattedInput.txt","r") as f:
     n_worker = int(f.readline().split()[0])
@@ -52,11 +55,18 @@ with open("FormattedInput.txt","r") as f:
 skill = np.array(skill) # Chain - Worker - Skill
 timetable = np.array(timetable) # Day - Chain - Shift
 chain_need = np.array(chain_need) # Chain - Skill
+shift_count = np.zeros((n_worker,2)) # Worker - Shift (Day - Night)
 # print(skill)
-print(timetable)
+# print(timetable)
 # print(chain_need)
 
 # --------------------------------
+# Create function
+def balance(day,night,worker):
+    return day + shift_count[worker,0] + (night + shift_count[worker,1]) * 1.2
+
+# --------------------------------
+# Model
 def ScheduleDay(prevSchedule,day):
     model = gp.Model(env=env) # Create Model
 
@@ -69,39 +79,56 @@ def ScheduleDay(prevSchedule,day):
     for chain in range(N_CHAIN):
         for sk in range(N_SKILL):
             for shift in range(N_SHIFT):
-                model.addConstr(lmao[chain,shift,:,sk].sum() == chain_need[chain,sk]) # 
+                model.addConstr(lmao[chain,shift,:,sk].sum() == chain_need[chain,sk]) # Đủ nhân lực cho từng công việc từng dây chuyền từng ca 
         for ppl in range(n_worker):
             for sk in range(N_SKILL):
-                model.addConstr(lmao[chain,:,ppl,sk].sum() <= skill[chain,ppl,sk])
+                model.addConstr(lmao[chain,:,ppl,sk].sum() <= skill[chain,ppl,sk]) # Nhân sự làm đúng kĩ năng
 
-    model.addConstrs(lmao[:,:,ppl,:].sum() <= 1 for ppl in range(n_worker))
-    model.addConstrs(lmao[:,0,ppl,:].sum() <= 1 - prevSchedule[:,2,ppl,:].sum() for ppl in range(n_worker))
+    model.addConstrs(lmao[:,:,ppl,:].sum() <= 1 for ppl in range(n_worker)) # Mỗi ngày làm 1 việc 1 ca 1 dây chuyền
+    model.addConstrs(lmao[:,0,ppl,:].sum() <= 1 - prevSchedule[:,2,ppl,:].sum() for ppl in range(n_worker)) # Làm ca 3 hôm trước ko làm ca 1 hôm sau
+
     
-    model.setObjective(lmao.sum(),sense = GRB.MINIMIZE)
+
+    # Set objective
+    balance_arr = [balance(lmao[:,:2,ppl,:].sum(),lmao[:,2,ppl,:].sum(),ppl) for ppl in range(n_worker)]
+    num_of_pair = int((n_worker / 2) * (n_worker - 1))
+    obj = 0
+    for id1 in range(n_worker):
+        for id2 in range(id1,n_worker):
+            obj = obj + (balance_arr[i] - balance_arr[j]) * (balance_arr[i] - balance_arr[j])
+        obj = obj / num_of_pair
+
+    model.setObjective(obj,sense = GRB.MINIMIZE)
 
     model.optimize()
 
     if model.status == GRB.OPTIMAL:
-        return lmao.x[0].astype(int)
+        return lmao.x.astype(int)
     else: return -1
     
 def main():
-    prevSchedule = np.zeros((3,3,17,3))
+    prevSchedule = np.zeros((3,3,n_worker,3))
     f = open("result_data_1_part_a.txt","w")
-    day = 1
-    res = ScheduleDay(prevSchedule=prevSchedule,day=day)
-    for chain in range(N_CHAIN):
-        for shift in range(N_SHIFT):
-            for sk in range(N_SKILL):
-                for ppl in range(n_worker):
-                    if res[chain,shift,ppl,sk]: 
-                        tmp = str(day)
-                        if len(tmp) == 1: tmp = "0" + tmp
-                        f.write(f"{tmp}.06.2023 Ca_{shift+1} {id_to_code[ppl]} Day_chuyen_{chain+1} {sk}")
+
+    for day in range(1,29):
+        res = ScheduleDay(prevSchedule=prevSchedule,day=day)
+        for chain in range(N_CHAIN):
+            for shift in range(N_SHIFT):
+                for sk in range(N_SKILL):
+                    for ppl in range(n_worker):
+                        if res[chain,shift,ppl,sk]: 
+                            tmp = str(day)
+                            if len(tmp) == 1: tmp = "0" + tmp
+                            f.write(f"{tmp}.06.2023 Ca_{shift+1} {id_to_code[ppl]} Day_chuyen_{chain+1} {sk}\n")
+        prevSchedule = res
+        for ppl in range(n_worker):
+            shift_count[ppl,0] += res[:,:2,ppl,:].sum()
+            shift_count[ppl,1] += res[:,2,ppl,:].sum()
+    
     f.close()
                         
                         
 
 if __name__ == "__main__":
-    # main()
+    main()
     ...
